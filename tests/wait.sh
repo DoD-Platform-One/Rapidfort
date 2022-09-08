f1cadee64e7180fe8cc2d716c5a8e1c7134afc33
Nrupen Desai <nrupen@rapidfort.com>
2022-09-08 16:39:59 -0700

    Adding fail case

--- tests/wait.sh
+++ tests/wait.sh
@@ -8,15 +8,15 @@ kubectl get deploy -n rapidfort
 
 kubectl get pods -n rapidfort
 
-kubectl get NetworkPolicy -n rapidfort
+kubectl get NetworkPolicy -n rapidfort || true
 
-kubectl get DestinationRule -n rapidfort
+kubectl get DestinationRule -n rapidfort || true
 
-kubectl get ServiceEntry -n rapidfort
+kubectl get ServiceEntry -n rapidfort || true
 
-kubectl get PeerAuthentication -n rapidfort
+kubectl get PeerAuthentication -n rapidfort || true
 
-kubectl get VirtualService -n rapidfort
+kubectl get VirtualService -n rapidfort || true
 
 kubectl describe pod -n rapidfort mysql-0
 set +x
