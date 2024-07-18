bdd483d52ec9d955e39683baff3088325a069feb
Nrupen Desai <nrupen@rapidfort.com>
2024-07-18 16:34:40 +0000

    Update istio config to make service local

--- utils/registrySync.sh
+++ utils/registrySync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pushd utils
-skopeo sync --src yaml --dest docker skopeo-rf-sync.yaml registry.dso.mil/platform-one/big-bang/apps/third-party/rapidfort/
+skopeo sync --src yaml --dest docker skopeo-rf-sync.yaml repo1.dso.mil/big-bang/product/community/rapidfort/
 popd
